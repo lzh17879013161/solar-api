cb08232a80f45cb670652683e3100de00ff883f2
lzh17879013161 <2486023660@qq.com>
2026-05-28 13:25:30 +0800

    Add files via upload

--- api/index.js
+++ api/index.js
@@ -1,9 +1,13 @@
 const jsonServer = require('json-server');
+const serverless = require('serverless-http');
+
+// 创建服务器
 const server = jsonServer.create();
-const router = jsonServer.router('../db.json');
+const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
 
-module.exports = server;
+// 适配Vercel无服务器环境
+module.exports.handler = serverless(server);
